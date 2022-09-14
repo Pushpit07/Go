1ac2564958f2e40a3ac338489a76b1a65964dba4
Pushpit <pushpit07@gmail.com>
2022-09-14 20:51:00 +0530

    loops and slices in go
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- cards/main.go
+++ cards/main.go
@@ -4,6 +4,18 @@ import "fmt"
 
 // Go Playground: https://play.golang.org/
 func main() {
+	// Slice
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	// fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func printCard() {
 	// var card string = "Ace of Spades"
 	card := "Ace of Spades" // Only use := when declaring new variable
 	card = "Five of Diamonds"
