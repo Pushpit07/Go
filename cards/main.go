b78e4dbf0d36ee336e0755e94dc229d53844b1f6
Pushpit <pushpit07@gmail.com>
2022-09-16 00:09:58 +0530

    New Deck function
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- cards/main.go
+++ cards/main.go
@@ -1,30 +1,8 @@
 package main
 
-import "fmt"
-
 // Go Playground: https://play.golang.org/
 func main() {
-	// Slice
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	// fmt.Println(cards)
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func printCard() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades" // Only use := when declaring new variable
-	card = "Five of Diamonds"
-
-	card = newCard()
-
-	fmt.Println(card)
-}
-
-func newCard() string {
-	return "Eight of Diamonds"
+	cards.print()
 }
