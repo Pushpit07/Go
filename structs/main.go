286030444ccefc32b69745dd4134d9c107d757b3
Pushpit <pushpit07@gmail.com>
2022-11-26 12:05:04 +0530

    Finished structs
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- structs/main.go
+++ structs/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
@@ -14,8 +20,26 @@ func main() {
 	var jake person
 	fmt.Println(jake)
 	fmt.Printf("%+v\n", jake)
-
 	jake.firstName = "Jake"
 	jake.lastName = "Jones"
 	fmt.Printf("%+v\n", jake)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
 }
